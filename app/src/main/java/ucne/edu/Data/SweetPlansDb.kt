4a896a7d2f353d7049e166ce3102e2389f09aa1e
Perla D. Canario Perez <89818496+PerlaCanario@users.noreply.github.com>
2022-11-22 18:58:47 -0400

    Agregando base de datos

---
+++ app/src/main/java/ucne/edu/Data/SweetPlansDb.kt
@@ -0,0 +1,19 @@
+package ucne.edu.Data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import ucne.edu.Data.Dao.UsuarioDao
+import ucne.edu.model.Usuario
+
+@Database(
+    entities = [
+        Usuario ::class ,
+        //Agenda :: class
+               ],
+    exportSchema = false,
+    version = 1
+)
+abstract class SweetPlansDb : RoomDatabase() {
+    abstract val usuarioDao: UsuarioDao
+    //abstract val RegistroAgendaDao : RegistroAgendaDao
+}
