155361520bad16144396b2b7923f067bee7f0371
Perla D. Canario Perez <89818496+PerlaCanario@users.noreply.github.com>
2022-11-23 01:41:39 -0400

    Agregando navegacion

--- app/src/main/java/ucne/edu/MainActivity.kt
+++ app/src/main/java/ucne/edu/MainActivity.kt
@@ -10,8 +10,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import ucne.edu.ui.Login.LoginScreen
+import ucne.edu.ui.Login.RegistroUsiario
+import ucne.edu.ui.navegacion.Screen
 import ucne.edu.ui.theme.ProyectoAplicada2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -30,14 +38,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MyApp(){
+    val navHostController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ProyectoAplicada2Theme {
-        Greeting("Android")
+    NavHost(navController = navHostController, startDestination = Screen.LoginScreen.route) {
+        composable(Screen.LoginScreen.route) {
+            LoginScreen(navHostController = navHostController)
+        }
+        composable(Screen.RegistroUsuarios.route) {
+            RegistroUsiario(navHostController = navHostController)
+        }
     }
 }
