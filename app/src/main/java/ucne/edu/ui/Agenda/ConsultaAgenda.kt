22b403dd3f2ae0462b111352c885a22b3d512e6d
Perla D. Canario Perez <89818496+PerlaCanario@users.noreply.github.com>
2022-12-07 18:51:39 -0400

    Modificando consulta

--- app/src/main/java/ucne/edu/ui/Agenda/ConsultaAgenda.kt
+++ app/src/main/java/ucne/edu/ui/Agenda/ConsultaAgenda.kt
@@ -4,6 +4,7 @@ import android.widget.CalendarView
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
